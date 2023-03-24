c1a3018adfc785d5195ffd16d6143e9ebf9c52f1
Tim Y Tian <574713572@qq.com>
2023-03-24 14:26:36 +0800

    Update serve_grobid.sh

--- serve_grobid.sh
+++ serve_grobid.sh
@@ -5,8 +5,8 @@ declare -r GROBID_VERSION="0.6.2" # or change to current stable version
 
 if [ ! -d grobid-${GROBID_VERSION} ]; then
   wget https://github.com/kermitt2/grobid/archive/${GROBID_VERSION}.zip
-  unzip "${GROBID_VERSION}.zip"
-  rm "${GROBID_VERSION}.zip"
+  unzip "grobid-${GROBID_VERSION}.zip"
+  rm "grobid-${GROBID_VERSION}.zip"
 fi
 
 # run GROBID
