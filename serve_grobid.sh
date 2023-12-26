04329affebeb7cfb87c088af002ac6ad645b7d75
YixinLiu <zxczxcz@qq.com>
2023-12-25 20:11:13 -0500

    upload

--- serve_grobid.sh
+++ serve_grobid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download GROBID if directory does not exist
-declare -r GROBID_VERSION="0.6.2" # or change to current stable version
+declare -r GROBID_VERSION="0.8.0" # or change to current stable version
 
 if [ ! -d grobid-${GROBID_VERSION} ]; then
   wget https://github.com/kermitt2/grobid/archive/${GROBID_VERSION}.zip
